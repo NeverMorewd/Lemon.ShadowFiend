ead2340f2fe272f09d4b12c1e1e06299d8c93f1d
Easley <nevermore.wd@me.com>
2024-11-14 16:10:37 +0800

    clean clear

---
+++ Lemon.ShadowFiend/Platform.Invoke.Win32/Kernel32/ErrorMessageHelper.cs
@@ -0,0 +1,6 @@
+namespace Platform.Invoke.Win32.Kernel32;
+
+public class ErrorMessageHelper
+{
+    
+}
---
+++ Lemon.ShadowFiend/Platform.Invoke.Win32/Kernel32/Kernel32Declaration.cs
@@ -0,0 +1,23 @@
+using System.Runtime.InteropServices;
+
+namespace Platform.Invoke.Win32.Kernel32;
+
+public class Kernel32Declaration
+{
+    [DllImport("advapi32.dll", SetLastError = true)]
+    private static extern bool LogonUser(string lpszUsername,
+        string lpszDomain,
+        string lpszPassword,
+        int dwLogonType,
+        int dwLogonProvider,
+        out IntPtr phToken
+    );
+
+    [DllImport("kernel32.dll")]
+    private static extern int FormatMessage(int dwFlags, ref IntPtr lpSource, int dwMessageId, int dwLanguageId,
+        ref String lpBuffer, int nSize, ref IntPtr Arguments);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool CloseHandle(IntPtr hObject);
+}
